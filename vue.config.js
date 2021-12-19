c12789f77f58723501f2dc9d668a2c3a92f6ce80
Minarox <mathis.sema@gmail.com>
2021-12-19 17:33:05 +0100

    Fix public path for github-pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/Workshop/'
+      : '/',
   devServer: {
     contentBase: path.join(__dirname, "public/"),
     port: 8080,
