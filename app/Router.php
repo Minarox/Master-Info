22ad564ca70b730594f79e94f2bab724c307bf17
Minarox <mathis.sema@gmail.com>
2022-01-15 11:13:25 +0100

    Create project

---
+++ app/Router.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace app;
+
+use Codes\ErrorCode;
+use Controllers\HelloController;
+use Slim\App;
+
+/**
+ * List of route for the app
+ * @param App $app
+ */
+return function (App $app) {
+    // CORS Policy
+    $app->options("/{routes:.+}", function ($request, $response) {
+        return $response;
+    });
+
+    $app->get("[/]", [HelloController::class, "helloWorld"]);
+
+    /**
+     * Redirect to 404 if none of the routes match
+     */
+    $app->map(["GET", "POST", "PUT", "DELETE", "PATCH"], "/{routes:.+}", function ($request, $response) {
+        return (new ErrorCode())->methodNotAllowed();
+    });
+};
