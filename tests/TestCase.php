aa53f5d6220e1911576714b42af3189ffd9d185c
Minarox <mathis.sema@gmail.com>
2022-01-16 18:17:11 +0100

    Add tests for SessionController

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 require_once __DIR__ . "/../app/Loader.php";
 
 use PHPUnit\Framework\TestCase as PHPUnit_TestCase;
+use app\Database;
+use Slim\Psr7\Factory\StreamFactory;
+use Slim\Psr7\Headers;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+use Slim\Psr7\Uri;
 
 /**
  * Tests case for app
@@ -11,12 +17,74 @@ use PHPUnit\Framework\TestCase as PHPUnit_TestCase;
 class TestCase extends PHPUnit_TestCase
 {
     /**
+     * @var PDO
+     */
+    protected PDO $pdo;
+
+    /**
+     * @var Response
+     */
+    protected Response $response;
+
+    /**
+     * @var array
+     */
+    protected array $test_user;
+
+    /**
+     * @param string $method
+     * @param string $path
+     * @param array $body
+     * @param bool $connected
+     * @return Request
+     */
+    protected function createRequest(string $method, string $path, array $body = [], bool $connected = true): Request
+    {
+        $uri = new Uri('', '', 80, $path);
+        $stream = (new StreamFactory())->createStream(json_encode($body));
+
+        $headers = new Headers();
+        $headers->addHeader("Content-type", "application/json");
+        $headers->addHeader("Cache-control", "no-cache");
+        if ($connected) $headers->addHeader("Authorization", $this->test_user["username"] . ' ' . $this->test_user["token"]);
+
+        return new Request($method, $uri, $headers, [], [], $stream);
+    }
+
+    /**
+     * Generate random string
+     *
+     * @param int $length of the generated string
+     * @return string
+     */
+    protected function randString(int $length = 16): string
+    {
+        // Password generator with custom length
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return substr(str_shuffle($chars), 0, $length);
+    }
+
+    /**
      * SetUp parameters before execute tests
      */
-    protected function setUp(): void {}
+    protected function setUp(): void {
+        $this->pdo = (new Database())->getPdo();
+        $this->pdo->query("DELETE FROM Users WHERE username = 'test_user_phpunit' AND is_admin = 1;")->execute();
+        $token = $this->randString(60);
+        $date = date("Y-m-d H:i:s", strtotime(date("Y-m-d H:i:s")) + $GLOBALS["config"]["session"]["token_lifetime"]);
+        $test_user_id = $this->pdo->query("INSERT INTO Users (username, is_admin, token, expire) VALUES ('test_user_phpunit', 1, '$token', '$date') RETURNING id;")->fetchColumn();
+        $this->test_user = $this->pdo->query("SELECT * FROM Users WHERE id = '$test_user_id' LIMIT 1;")->fetch();
+        $this->response = new Response();
+    }
 
     /**
      * Clean parameters after execute tests
      */
-    protected function tearDown(): void {}
+    protected function tearDown(): void {
+        unset($this->response);
+        $test_user_id = $this->test_user["id"];
+        $this->pdo->query("DELETE FROM Users WHERE id = '$test_user_id';")->execute();
+        unset($this->test_user);
+        unset($this->pdo);
+    }
 }
---
+++ tests/TestCase/Controllers/SessionControllerTest.php
@@ -0,0 +1,181 @@
+<?php
+declare(strict_types=1);
+
+namespace TestCase\Controllers;
+
+require_once __DIR__ . "/../../TestCase.php";
+
+use Controllers\SessionController;
+use BadRequest;
+use NotFound;
+use TestCase;
+
+/**
+ * Test class for SessionController
+ */
+class SessionControllerTest extends TestCase
+{
+    /**
+     * @var SessionController
+     */
+    private SessionController $sessionController;
+
+    /**
+     * Test login function
+     *
+     * @throws BadRequest|NotFound
+     */
+    public function testLogin()
+    {
+        $test_user_id = $this->test_user["id"];
+        $request = $this->createRequest("POST", "/login", ["username" => "test_user_phpunit"], false);
+        $result = $this->sessionController->login($request, $this->response);
+
+        self::assertSame(
+            json_encode($this->pdo->query("SELECT * FROM Users WHERE id = '$test_user_id' LIMIT 1;")->fetch()),
+            $result->getBody()->__toString()
+        );
+        self::assertSame(200, $result->getStatusCode());
+    }
+
+    /**
+     * Test login function without account
+     *
+     * @throws BadRequest|NotFound
+     */
+    public function testLoginWithoutAccount()
+    {
+        $request = $this->createRequest("POST", "/login", ["username" => "test_user_phpunit2"], false);
+        $result = $this->sessionController->login($request, $this->response);
+        $test_user_id = (json_decode($result->getBody()->__toString(), true))["id"];
+
+        self::assertSame(
+            json_encode($this->pdo->query("SELECT * FROM Users WHERE id = '$test_user_id' LIMIT 1;")->fetch()),
+            $result->getBody()->__toString()
+        );
+        self::assertSame(200, $result->getStatusCode());
+
+        $this->pdo->prepare("DELETE FROM Users WHERE id = '$test_user_id';")->execute();
+    }
+
+    /**
+     * Test login function without account with blocked by maxUsers
+     *
+     * @throws BadRequest|NotFound
+     */
+    public function testLoginWithoutAccountBlocked()
+    {
+        $temp = $GLOBALS["config"]["session"]["maxUsers"];
+        $GLOBALS["config"]["session"]["maxUsers"] = 0;
+
+        $request = $this->createRequest("POST", "/login", ["username" => "test_user_phpunit2"], false);
+        $result = $this->sessionController->login($request, $this->response);
+
+        self::assertSame(
+            json_encode([
+                "code_value" => 409,
+                "code_description" => "Maximum number of users reached"
+            ]),
+            $result->getBody()->__toString()
+        );
+        self::assertSame(409, $result->getStatusCode());
+
+        $GLOBALS["config"]["session"]["maxUsers"] = $temp;
+    }
+
+    /**
+     * Test session function
+     *
+     * @throws BadRequest|NotFound
+     */
+    public function testSession()
+    {
+        $request = $this->createRequest("GET", "/session");
+        $result = $this->sessionController->currentSession($request, $this->response);
+        $test_user_id = $this->test_user["id"];
+
+        self::assertSame(
+            json_encode($this->pdo->query("SELECT * FROM Users WHERE id = '$test_user_id' LIMIT 1;")->fetch()),
+            $result->getBody()->__toString()
+        );
+        self::assertSame(200, $result->getStatusCode());
+    }
+
+    /**
+     * Test session function without token
+     *
+     * @throws BadRequest|NotFound
+     */
+    public function testSessionWithoutToken()
+    {
+        $request = $this->createRequest("GET", "/session", connected: false);
+        $result = $this->sessionController->currentSession($request, $this->response);
+
+        self::assertSame(
+            json_encode([
+                "code_value" => 401,
+                "code_description" => "Unauthorized"
+            ]),
+            $result->getBody()->__toString()
+        );
+        self::assertSame(401, $result->getStatusCode());
+    }
+
+    /**
+     * Test logout function
+     *
+     * @throws BadRequest|NotFound
+     */
+    public function testLogout()
+    {
+        $request = $this->createRequest("GET", "/logout");
+        $result = $this->sessionController->logout($request, $this->response);
+
+        self::assertSame(
+            json_encode([
+                "code_value" => 200,
+                "code_description" => "Success"
+            ]),
+            $result->getBody()->__toString()
+        );
+        self::assertSame(200, $result->getStatusCode());
+    }
+
+    /**
+     * Test logout function without token
+     *
+     * @throws BadRequest|NotFound
+     */
+    public function testLogoutWithoutToken()
+    {
+        $request = $this->createRequest("GET", "/logout", connected: false);
+        $result = $this->sessionController->logout($request, $this->response);
+
+        self::assertSame(
+            json_encode([
+                "code_value" => 401,
+                "code_description" => "Unauthorized"
+            ]),
+            $result->getBody()->__toString()
+        );
+        self::assertSame(401, $result->getStatusCode());
+    }
+
+    /**
+     * SetUp parameters before execute tests
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->sessionController = new SessionController();
+    }
+
+    /**
+     * Clean parameters after execute tests
+     */
+    protected function tearDown(): void
+    {
+        unset($this->sessionController);
+        parent::tearDown();
+    }
+}
