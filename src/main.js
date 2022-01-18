7a77ab122148b4928d375bc29fc06522be1392e5
Minarox <mathis.sema@gmail.com>
2022-01-19 00:03:13 +0100

    Add global data and methods

--- src/main.js
+++ src/main.js
@@ -2,4 +2,28 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+app.mixin({
+    data() {
+        return {
+            session: [],
+            api_url: "https://ic.minarox.fr/api"
+        }
+    },
+    methods: {
+        checkSessionExist() {
+            if (localStorage.getItem("session")) router.push('/');
+        },
+        isAdmin(routerPush = true) {
+            let admin = JSON.parse(localStorage.getItem("session"))["is_admin"].toString();
+            if (admin !== '1') {
+                if (routerPush) this.$router.push('/');
+                return false;
+            }
+            return true;
+        }
+    }
+});
+
+app.use(router).mount("#app");
