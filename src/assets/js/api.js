3fdff8b2e403eef98da3bac3a10ed7c65f81a4aa
Minarox <mathis.sema@gmail.com>
2022-01-19 16:37:07 +0100

    Add all endpoints

--- src/assets/js/api.js
+++ src/assets/js/api.js
@@ -4,23 +4,23 @@ const api_url = "https://ic.minarox.fr/api";
 
 export const API = {
     login,
-    // currentSession,
+    currentSession,
     logout,
-    //
-    // getConfig,
-    // setMaxUsers,
-    // setUsersPerGroup,
-    // setLastGroupConfig,
-    // deleteUser,
-    // getGroups,
-    // deleteGroup,
-    //
-    // getCurrentGroup,
-    // addGroup,
+
+    getConfig,
+    setMaxUsers,
+    setUsersPerGroup,
+    setLastGroupConfig,
+    deleteUser,
+    getGroups,
+    deleteGroup,
+
+    getCurrentGroup,
+    addGroup,
     getUsers,
-    // leaveCurrentGroup,
-    // joinRandomGroup,
-    // joinGroup
+    leaveCurrentGroup,
+    joinRandomGroup,
+    joinGroup
 };
 
 function headers() {
@@ -43,9 +43,15 @@ function headers() {
 }
 
 function login(username) {
-    const data = {username: username};
+    return axios.post(api_url + "/login", {username: username}, headers())
+        .then(response => {
+            localStorage.setItem("session", JSON.stringify(response.data));
+            return response.data;
+        });
+}
 
-    return axios.post(api_url + "/login", data, headers())
+function currentSession() {
+    return axios.get(api_url + "/session", headers())
         .then(response => {
             localStorage.setItem("session", JSON.stringify(response.data));
             return response.data;
@@ -56,16 +62,103 @@ function logout() {
     return axios.get(api_url + "/logout", headers())
         .then(response => {
             localStorage.removeItem("session");
+            localStorage.removeItem("group");
             return response.data;
         })
         .catch(error => {
             localStorage.removeItem("session");
+            localStorage.removeItem("group");
             return error;
+        });
+}
+
+function getConfig() {
+    return axios.get(api_url + "/admin/config", headers())
+        .then(response => {
+            return response.data;
+        });
+}
+
+function setMaxUsers(max_users) {
+    return axios.post(api_url + "/admin/max-users", {max_users: max_users}, headers())
+        .then(response => {
+            return response.data;
+        });
+}
+
+function setUsersPerGroup(users_per_group) {
+    return axios.post(api_url + "/admin/users-per-group", {users_per_group: users_per_group}, headers())
+        .then(response => {
+            return response.data;
+        });
+}
+
+function setLastGroupConfig(last_group_mode) {
+    return axios.post(api_url + "/admin/last-group", {last_group_mode: last_group_mode}, headers())
+        .then(response => {
+            return response.data;
+        });
+}
+
+function deleteUser(user_id) {
+    return axios.delete(api_url + "/admin/user/" + user_id, headers())
+        .then(response => {
+            return response.data;
+        });
+}
+
+function getGroups() {
+    return axios.get(api_url + "/admin/groups", headers())
+        .then(response => {
+            return response.data;
+        });
+}
+
+function deleteGroup(group_id) {
+    return axios.delete(api_url + "/admin/group/" + group_id, headers())
+        .then(response => {
+            return response.data;
+        });
+}
+
+function getCurrentGroup() {
+    return axios.get(api_url + "/group", headers())
+        .then(response => {
+            localStorage.setItem("group", JSON.stringify(response.data));
+            return response.data;
+        });
+}
+
+function addGroup(name) {
+    return axios.post(api_url + "/group", {name: name}, headers())
+        .then(response => {
+            return response.data;
         });
 }
 
 function getUsers() {
     return axios.get(api_url + "/group/users", headers())
+        .then(response => {
+            return response.data;
+        });
+}
+
+function leaveCurrentGroup() {
+    return axios.get(api_url + "/group/leave", headers())
+        .then(response => {
+            return response.data;
+        });
+}
+
+function joinRandomGroup() {
+    return axios.get(api_url + "/group/random", headers())
+        .then(response => {
+            return response.data;
+        });
+}
+
+function joinGroup(group_link) {
+    return axios.get(api_url + "/group/join/" + group_link, headers())
         .then(response => {
             return response.data;
         });
