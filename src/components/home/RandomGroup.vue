d85ac265953c82fb10a8ad2958ca437d6f12e7c9
Minarox <mathis.sema@gmail.com>
2022-01-20 14:55:52 +0100

    Add RandomGroup component

---
+++ src/components/home/RandomGroup.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="popup">
+    <article>
+      <header>
+        <h2>Rejoindre aléatoirement un groupe</h2>
+      </header>
+
+      <section>
+        <p>Voulez vous vraiment rejoindre un groupe aléatoirement ?</p>
+        <div>
+          <button type="submit" class="button">Oui</button>
+          <button type="button" class="button btn-back" @click="$emit('component', {name: ''})">Non</button>
+        </div>
+      </section>
+    </article>
+  </div>
+</template>
+
+<script>
+import {API} from "../../assets/js/api";
+
+export default {
+  name: "RandomGroup",
+  mounted() {
+    this.addEvents('', document.getElementsByClassName("popup")[0]);
+  },
+  beforeUnmount() {
+    this.removeEvents('', document.getElementsByClassName("popup")[0]);
+  },
+  methods: {
+    randomGroup() {
+      API.joinRandomGroup().then(() => {
+        this.$router.go(0);
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.popup {
+  animation: fade-in .4s alternate;
+}
+
+.popup article section p {
+  text-align: center;
+  margin-bottom: 22px;
+}
+
+.popup article section div {
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: center;
+  gap: 20px 40px;
+}
+
+.popup article section div button {
+  margin: 0;
+}
+
+@keyframes fade-in {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
