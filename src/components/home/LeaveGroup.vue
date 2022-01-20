d012aa2dcba2b36605713cabe452c974c8cf5da4
Minarox <mathis.sema@gmail.com>
2022-01-20 14:55:45 +0100

    Add LeaveGroup component

---
+++ src/components/home/LeaveGroup.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="popup">
+    <article>
+      <header>
+        <h2>Quitter le groupe</h2>
+      </header>
+
+      <section>
+        <p>Voulez vous vraiment quitter le groupe ?</p>
+        <div>
+          <button type="submit" class="button btn-warning" @click="leaveGroup">Valider</button>
+          <button type="button" class="button btn-back" @click="$emit('component', {name: ''})">Retour</button>
+        </div>
+      </section>
+    </article>
+  </div>
+</template>
+
+<script>
+import {API} from "../../assets/js/api";
+
+export default {
+  name: "LeaveGroup",
+  mounted() {
+    this.addEvents('', document.getElementsByClassName("popup")[0]);
+  },
+  beforeUnmount() {
+    this.removeEvents('', document.getElementsByClassName("popup")[0]);
+  },
+  methods: {
+    leaveGroup() {
+      API.leaveCurrentGroup().then(() => {
+        this.$router.go(0);
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.popup {
+  animation: fade-in .4s alternate;
+}
+
+.popup article section p {
+  text-align: center;
+  margin-bottom: 22px;
+}
+
+.popup article section div {
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: center;
+  gap: 20px 40px;
+}
+
+.popup article section div button {
+  margin: 0;
+}
+
+@keyframes fade-in {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
