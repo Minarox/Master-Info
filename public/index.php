49c237fc230e213d8196b2bb11d011024729c739
Mathis <contact@minarox.fr>
2022-04-12 12:41:09 +0200

    Improve PHP errors

--- public/index.php
+++ public/index.php
@@ -9,6 +9,10 @@ if (CONFIG["debug"]["displayPHPErrors"]) {
     ini_set("display_errors", '1');
     ini_set("display_startup_errors", '1');
     error_reporting(E_ALL);
+} else {
+    ini_set("display_errors", '0');
+    ini_set("display_startup_errors", '0');
+    error_reporting(0);
 }
 
 use Codes\ErrorCode;
