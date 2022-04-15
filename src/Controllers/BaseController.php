6a0b42e15a7dcabe920738015129963d24d81c67
Mathis <contact@minarox.fr>
2022-04-15 13:43:27 +0200

    Bump to v0.3

--- src/Controllers/BaseController.php
+++ src/Controllers/BaseController.php
@@ -25,7 +25,7 @@ class BaseController extends Controller
         // Display description
         $response->getBody()->write(
             json_encode([
-                "version"     => "v0.2",
+                "version"     => "v0.3",
                 "title"       => "Cerealis API",
                 "description" => "Enterprise Resource Management API",
                 "host"        => "https://mspr.minarox.fr",
