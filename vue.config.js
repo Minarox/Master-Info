b1c9a17f9024d383e671a770af52ed9849c57c08
Mathis <contact@minarox.fr>
2022-05-01 22:26:51 +0200

    Change server port

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   devServer: {
     contentBase: path.join(__dirname, "public/"),
-    port: 8101,
+    port: 8100,
     public: "mspr.minarox.fr",
     disableHostCheck: true,
   },
