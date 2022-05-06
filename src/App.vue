65e57af60ab9e7b75de4d2eb0deb0740c423d5cb
Mathis <contact@minarox.fr>
2022-05-06 02:10:17 +0200

    Rewrite CSS

--- src/App.vue
+++ src/App.vue
@@ -72,7 +72,7 @@ body {
 
 main {
   height: calc(100vh - 64px);
-  overflow: auto;
+  overflow: auto; /* TODO: remove at the end */
 }
 
 main > div {
@@ -80,6 +80,13 @@ main > div {
   max-width: 1200px;
   margin: 30px auto;
   padding: 0 12px;
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 30px;
+}
+
+main div > article {
+  flex: 1 0 0 !important;
 }
 
 form label {
@@ -106,7 +113,7 @@ form button,
   margin: 0 auto;
   outline: none;
   cursor: pointer;
-  border-radius: 3px;
+  border-radius: 8px;
   font-weight: 500;
   line-height: 16px;
   padding: 10px 24px;
@@ -156,6 +163,16 @@ form button > * {
   flex-flow: row nowrap;
   align-items: center;
   justify-content: center;
+  animation: fade-in 0.4s alternate;
+}
+
+@keyframes fade-in {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 }
 
 .popup article {
@@ -186,12 +203,200 @@ form button > * {
   max-height: calc(100vh - 140px);
 }
 
+.popup article > section form div {
+  margin-top: 20px;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-evenly;
+  gap: 12px 40px;
+}
+
+.popup article > section form div button {
+  flex: 0 1 30%;
+  margin: 0;
+}
+
+@media only screen and (max-width: 500px) {
+  .popup article section form div {
+    flex-flow: column nowrap;
+    justify-content: center;
+  }
+
+  .popup article section form div button {
+    width: 100%;
+  }
+}
+
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.4s;
+  transition: opacity 0.3s;
 }
 .fade-enter,
 .fade-leave-to {
   opacity: 0;
+}
+
+.svg-button {
+  margin: 0;
+  width: 36px;
+  height: 36px;
+  padding: 7px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  border-radius: 8px;
+  transition: all .2s;
+  box-shadow: 0 3.2px 7.2px 0 rgb(0 0 0 / 13%),
+  0 0.6px 1.8px 0 rgb(0 0 0 / 11%);
+  fill: var(--title);
+}
+
+.svg-button:hover {
+  background-color: #dedede;
+}
+
+#filter form {
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: end;
+  justify-content: space-between;
+  gap: 20px;
+}
+
+#filter form div {
+  flex: 1 0 0;
+}
+
+#filter form label {
+  color: var(--bg-content);
+}
+
+#filter form input {
+  margin-bottom: 0;
+  border: none;
+  box-shadow: 0 3.2px 7.2px 0 rgb(0 0 0 / 13%),
+  0 0.6px 1.8px 0 rgb(0 0 0 / 11%);
+}
+
+#main {
+  background-color: var(--bg-content);
+  border-radius: 8px;
+  padding: 16px;
+  box-shadow: 0 3.2px 7.2px 0 rgb(0 0 0 / 13%),
+  0 0.6px 1.8px 0 rgb(0 0 0 / 11%);
+}
+
+#main header {
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 16px;
+}
+
+#main header h2 {
+  font-size: 1.3em;
+  font-weight: 500;
+  color: var(--title);
+}
+
+#main header div {
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-between;
+  gap: 16px;
+}
+
+#main header div a {
+  box-shadow: inset 0 3.2px 7.2px 0 rgb(0 0 0 / 13%),
+  inset 0 0.6px 1.8px 0 rgb(0 0 0 / 11%);
+  fill: var(--title);
+}
+
+#main header div a:hover {
+  box-shadow: inset 0 3.2px 7.2px 0 rgb(0 0 0 / 13%),
+  inset 0 0.6px 1.8px 0 rgb(0 0 0 / 11%);
+}
+
+#main section {
+  border-radius: 8px !important;
+  box-shadow: 0 3.2px 7.2px 0 rgb(0 0 0 / 13%),
+  0 0.6px 1.8px 0 rgb(0 0 0 / 11%);
+  overflow: auto;
+}
+
+#main table {
+  width: 100%;
+  border-spacing: 0;
+}
+
+#main table thead tr {
+  position: sticky;
+  top: 0;
+  background-color: var(--bg-content);
+  box-shadow: 0 3.2px 7.2px 0 rgb(0 0 0 / 13%),
+  0 0.6px 1.8px 0 rgb(0 0 0 / 11%);
+}
+
+#main table thead th {
+  padding: 10px;
+  text-align: left;
+  background-color: var(--bg-color-center);
+  color: white;
+  font-size: 1.1em;
+}
+
+#main table thead th:first-of-type {
+  border-radius: 8px 0 0 0;
+}
+
+#main table thead th:last-of-type {
+  border-radius: 0 8px 0 0;
+}
+
+#main table tbody td {
+  padding: 8px;
+}
+
+#main table tbody td:last-of-type {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+#main table tbody td:last-of-type > * {
+  display: inline-block;
+  object-fit: cover;
+  width: 22px;
+  height: 22px;
+  margin-right: 6px;
+  fill: var(--title);
+}
+
+#main table tbody tr td[colspan='4'] {
+  text-align: center;
+  background-color: var(--bg-content);
+  padding: calc((100vh / 2) - 64px - 30px * 2) 0;
+}
+
+#main table tbody tr:nth-of-type(odd) {
+  background-color: rgba(0, 0, 0, .05);
+}
+
+#buttons {
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: space-evenly;
+  gap: 30px;
+}
+
+#buttons > * {
+  flex: 0 0 30%;
+  box-shadow: 0 3.2px 7.2px 0 rgb(0 0 0 / 13%),
+  0 0.6px 1.8px 0 rgb(0 0 0 / 11%);
 }
 </style>
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -13,7 +13,7 @@
 
     <main>
       <div>
-        <article>
+        <article id="filter">
           <form>
             <div>
               <label for="email">Email</label>
@@ -31,53 +31,73 @@
               <label for="device">Appareil</label>
               <input type="text" name="device" id="device">
             </div>
-            <div>
-              <button class="button" type="submit">Rechercher</button>
-              <button class="button btn-back" type="reset">Réinitialiser</button>
-            </div>
+            <button class="svg-button" type="submit">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z"/></svg>
+            </button>
+            <button class="svg-button" type="reset">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM394.8 466.1C393.2 492.3 372.3 512 346.9 512H101.1C75.75 512 54.77 492.3 53.19 466.1L31.1 128H416L394.8 466.1z"/></svg>
+            </button>
           </form>
         </article>
 
-        <article>
-          <table>
-            <!-- Head -->
-            <thead>
-            <tr>
-              <th>&nbsp;</th>
-              <th>Email</th>
-              <th>Nom complet</th>
-              <th>Appareil</th>
-              <th>Ajouté le</th>
-              <th>Actions</th>
-            </tr>
-            </thead>
-            <!-- Content -->
-            <tbody v-if="users">
-            <tr v-for="user in users" :key="user['user_id']">
-              <td><input type="checkbox" name="name1" />&nbsp;</td>
-              <td>{{ user["email"] }}</td>
-              <td>{{ user["first_name"] + ' ' + user["last_name"] }}</td>
-              <td>{{ user["device"] }}</td>
-              <td>{{ user["created_at"].split('.')[0] }}</td>
-              <td>
-                <a href="" @click.prevent="selected_user = user; component = 'EditUser'">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M490.3 40.4C512.2 62.27 512.2 97.73 490.3 119.6L460.3 149.7L362.3 51.72L392.4 21.66C414.3-.2135 449.7-.2135 471.6 21.66L490.3 40.4zM172.4 241.7L339.7 74.34L437.7 172.3L270.3 339.6C264.2 345.8 256.7 350.4 248.4 353.2L159.6 382.8C150.1 385.6 141.5 383.4 135 376.1C128.6 370.5 126.4 361 129.2 352.4L158.8 263.6C161.6 255.3 166.2 247.8 172.4 241.7V241.7zM192 63.1C209.7 63.1 224 78.33 224 95.1C224 113.7 209.7 127.1 192 127.1H96C78.33 127.1 64 142.3 64 159.1V416C64 433.7 78.33 448 96 448H352C369.7 448 384 433.7 384 416V319.1C384 302.3 398.3 287.1 416 287.1C433.7 287.1 448 302.3 448 319.1V416C448 469 405 512 352 512H96C42.98 512 0 469 0 416V159.1C0 106.1 42.98 63.1 96 63.1H192z"/></svg>
-                </a>
-                <a href="" @click.prevent="selected_user = user; component = 'EditUser'">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M464 64C490.5 64 512 85.49 512 112C512 127.1 504.9 141.3 492.8 150.4L275.2 313.6C263.8 322.1 248.2 322.1 236.8 313.6L19.2 150.4C7.113 141.3 0 127.1 0 112C0 85.49 21.49 64 48 64H464zM217.6 339.2C240.4 356.3 271.6 356.3 294.4 339.2L512 176V384C512 419.3 483.3 448 448 448H64C28.65 448 0 419.3 0 384V176L217.6 339.2z"/></svg>
-                </a>
-                <a href="" @click.prevent="selected_user = user; component = 'DeleteUser'">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM394.8 466.1C393.2 492.3 372.3 512 346.9 512H101.1C75.75 512 54.77 492.3 53.19 466.1L31.1 128H416L394.8 466.1z"/></svg>
-                </a>
-              </td>
-            </tr>
-            </tbody>
-            <tbody v-else>
-            <tr>
-              <td colspan="4">Rien n'a été trouvé.</td>
-            </tr>
-            </tbody>
-          </table>
+        <article id="main">
+          <header>
+            <h2>Utilisateurs enregistrés</h2>
+            <div>
+              <a href="" @click.prevent="" class="svg-button">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M496 48V192c0 17.69-14.31 32-32 32H320c-17.69 0-32-14.31-32-32s14.31-32 32-32h63.39c-29.97-39.7-77.25-63.78-127.6-63.78C167.7 96.22 96 167.9 96 256s71.69 159.8 159.8 159.8c34.88 0 68.03-11.03 95.88-31.94c14.22-10.53 34.22-7.75 44.81 6.375c10.59 14.16 7.75 34.22-6.375 44.81c-39.03 29.28-85.36 44.86-134.2 44.86C132.5 479.9 32 379.4 32 256s100.5-223.9 223.9-223.9c69.15 0 134 32.47 176.1 86.12V48c0-17.69 14.31-32 32-32S496 30.31 496 48z"/></svg>
+              </a>
+              <a href="" @click.prevent="" class="svg-button">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z"/></svg>
+              </a>
+            </div>
+          </header>
+          <section>
+            <table>
+              <!-- Head -->
+              <thead>
+              <tr>
+                <th>&nbsp;</th>
+                <th>Email</th>
+                <th>Nom complet</th>
+                <th>Appareil</th>
+                <th>Ajouté le</th>
+                <th>Actions</th>
+              </tr>
+              </thead>
+              <!-- Content -->
+              <tbody v-if="users">
+              <tr v-for="user in users" :key="user['user_id']">
+                <td><input type="checkbox" name="name1" />&nbsp;</td>
+                <td>{{ user["email"] }}</td>
+                <td>{{ user["first_name"] + ' ' + user["last_name"] }}</td>
+                <td>{{ user["device"] }}</td>
+                <td>{{ user["created_at"].split('.')[0] }}</td>
+                <td>
+                  <a href="" @click.prevent="selected_user = user; component = 'EditUser'">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M490.3 40.4C512.2 62.27 512.2 97.73 490.3 119.6L460.3 149.7L362.3 51.72L392.4 21.66C414.3-.2135 449.7-.2135 471.6 21.66L490.3 40.4zM172.4 241.7L339.7 74.34L437.7 172.3L270.3 339.6C264.2 345.8 256.7 350.4 248.4 353.2L159.6 382.8C150.1 385.6 141.5 383.4 135 376.1C128.6 370.5 126.4 361 129.2 352.4L158.8 263.6C161.6 255.3 166.2 247.8 172.4 241.7V241.7zM192 63.1C209.7 63.1 224 78.33 224 95.1C224 113.7 209.7 127.1 192 127.1H96C78.33 127.1 64 142.3 64 159.1V416C64 433.7 78.33 448 96 448H352C369.7 448 384 433.7 384 416V319.1C384 302.3 398.3 287.1 416 287.1C433.7 287.1 448 302.3 448 319.1V416C448 469 405 512 352 512H96C42.98 512 0 469 0 416V159.1C0 106.1 42.98 63.1 96 63.1H192z"/></svg>
+                  </a>
+                  <a href="" @click.prevent="selected_user = user; component = 'EditUser'">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M464 64C490.5 64 512 85.49 512 112C512 127.1 504.9 141.3 492.8 150.4L275.2 313.6C263.8 322.1 248.2 322.1 236.8 313.6L19.2 150.4C7.113 141.3 0 127.1 0 112C0 85.49 21.49 64 48 64H464zM217.6 339.2C240.4 356.3 271.6 356.3 294.4 339.2L512 176V384C512 419.3 483.3 448 448 448H64C28.65 448 0 419.3 0 384V176L217.6 339.2z"/></svg>
+                  </a>
+                  <a href="" @click.prevent="selected_user = user; component = 'DeleteUser'">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM394.8 466.1C393.2 492.3 372.3 512 346.9 512H101.1C75.75 512 54.77 492.3 53.19 466.1L31.1 128H416L394.8 466.1z"/></svg>
+                  </a>
+                </td>
+              </tr>
+              </tbody>
+              <tbody v-else>
+              <tr>
+                <td colspan="4">Rien n'a été trouvé.</td>
+              </tr>
+              </tbody>
+            </table>
+          </section>
+        </article>
+
+        <article id="buttons">
+          <a href="" @click.prevent="" class="button">Envoyer un mail</a>
+          <a href="" @click.prevent="" class="button btn-warning">Supprimer les utilisateurs</a>
         </article>
       </div>
     </main>
@@ -119,84 +139,7 @@ export default {
 </script>
 
 <style scoped>
-main > div {
-  display: flex;
-  flex-flow: column nowrap;
-  gap: 30px;
-}
-
-article {
-  border-radius: 6px;
-  background-color: var(--bg-content);
-  box-shadow: rgba(149, 157, 165, 0.2) 0 8px 24px 0;
-}
-
-article:last-of-type {
-  overflow: auto;
-  height: calc(100vh - 64px - 122px - 30px * 3);
-}
-
-form {
-  padding: 20px;
-  display: flex;
-  flex-flow: row nowrap !important;
-  gap: 20px;
-}
-
-form button {
-  margin: 0;
-}
-
-form > * {
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: center;
-  width: 100%;
-  gap: 10px;
-}
-
-table {
-  width: 100%;
-  border-spacing: 0;
-}
-
-table thead tr {
-  position: sticky;
-  top: 0;
-  background-color: var(--bg-content-header);
-  box-shadow: 0 3.2px 7.2px 0 rgb(0 0 0 / 13%),
-  0 0.6px 1.8px 0 rgb(0 0 0 / 11%);
-}
-
-table thead th {
-  padding: 10px;
-  text-align: left;
-}
-
-table tbody td {
-  padding: 8px;
-}
-
-table tbody td:last-of-type {
-  display: flex;
-  align-items: center;
-  gap: 12px;
-}
-
-table tbody td:last-of-type > * {
-  display: inline-block;
-  object-fit: cover;
-  width: 22px;
-  height: 22px;
-}
-
-table tbody tr td[colspan='4'] {
-  text-align: center;
-  background-color: var(--bg-content);
-  padding: calc((100vh / 2) - 64px - 30px * 2) 0;
-}
-
-table tbody tr:nth-of-type(odd) {
-  background-color: rgba(0, 0, 0, .05);
+#main section {
+  height: calc(100vh - 365px);
 }
 </style>
