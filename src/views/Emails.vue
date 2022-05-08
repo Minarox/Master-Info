8c89419143f459e83d8d99c7fc120cea8650478e
Mathis <contact@minarox.fr>
2022-05-08 14:22:48 +0200

    Add cloneEmail component

--- src/views/Emails.vue
+++ src/views/Emails.vue
@@ -63,6 +63,9 @@
                 <td>{{ email["description"] }}</td>
                 <td>{{ email["created_at"].split('.')[0] }}</td>
                 <td>
+                  <a href="" @click.prevent="selected_email = email; component = 'CloneEmail'">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M384 96L384 0h-112c-26.51 0-48 21.49-48 48v288c0 26.51 21.49 48 48 48H464c26.51 0 48-21.49 48-48V128h-95.1C398.4 128 384 113.6 384 96zM416 0v96h96L416 0zM192 352V128h-144c-26.51 0-48 21.49-48 48v288c0 26.51 21.49 48 48 48h192c26.51 0 48-21.49 48-48L288 416h-32C220.7 416 192 387.3 192 352z"/></svg>
+                  </a>
                   <a href="" @click.prevent="selected_email = email; component = 'EditEmail'">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M490.3 40.4C512.2 62.27 512.2 97.73 490.3 119.6L460.3 149.7L362.3 51.72L392.4 21.66C414.3-.2135 449.7-.2135 471.6 21.66L490.3 40.4zM172.4 241.7L339.7 74.34L437.7 172.3L270.3 339.6C264.2 345.8 256.7 350.4 248.4 353.2L159.6 382.8C150.1 385.6 141.5 383.4 135 376.1C128.6 370.5 126.4 361 129.2 352.4L158.8 263.6C161.6 255.3 166.2 247.8 172.4 241.7V241.7zM192 63.1C209.7 63.1 224 78.33 224 95.1C224 113.7 209.7 127.1 192 127.1H96C78.33 127.1 64 142.3 64 159.1V416C64 433.7 78.33 448 96 448H352C369.7 448 384 433.7 384 416V319.1C384 302.3 398.3 287.1 416 287.1C433.7 287.1 448 302.3 448 319.1V416C448 469 405 512 352 512H96C42.98 512 0 469 0 416V159.1C0 106.1 42.98 63.1 96 63.1H192z"/></svg>
                   </a>
@@ -90,6 +93,7 @@ import Header from "@/components/global/Header";
 import UserInfo from "@/components/session/UserInfo";
 import EditPassword from "@/components/session/EditPassword";
 import AddEmail from "@/components/emails/AddEmail";
+import CloneEmail from "@/components/emails/CloneEmail";
 import EditEmail from "@/components/emails/EditEmail";
 import DeleteEmail from "@/components/emails/DeleteEmail";
 import {API} from "@/assets/js/api";
@@ -101,6 +105,7 @@ export default {
     UserInfo,
     EditPassword,
     AddEmail,
+    CloneEmail,
     EditEmail,
     DeleteEmail
   },
