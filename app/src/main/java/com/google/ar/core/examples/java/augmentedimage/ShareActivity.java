7e96c820e369b4bcca24c8e639c8a04496b64ea6
Mathis <contact@minarox.fr>
2022-05-16 21:59:28 +0200

    beta v1

---
+++ app/src/main/java/com/google/ar/core/examples/java/augmentedimage/ShareActivity.java
@@ -0,0 +1,35 @@
+package com.google.ar.core.examples.java.augmentedimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.File;
+
+public class ShareActivity extends AppCompatActivity {
+
+    private String ImgString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+        ImageView ShowView = findViewById(R.id.ShowImg);
+
+        Intent intent = getIntent();
+        ImgString = intent.getStringExtra("Name");
+
+        File imgFile = new File(Environment.getExternalStorageDirectory(), "Pictures/Ar_Core/" + ImgString);
+
+        Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+
+        ShowView.setImageBitmap(myBitmap);
+
+    }
+}
