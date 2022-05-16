2fae0230adc9d48b61b4244d31f65a674fe81e16
Mathis <contact@minarox.fr>
2022-05-16 23:37:52 +0200

    Bump to v1.4.0

--- tests/Controllers/BaseControllerTest.php
+++ tests/Controllers/BaseControllerTest.php
@@ -49,7 +49,7 @@ class BaseControllerTest extends TestCase
         // Check if request = database and http code is correct
         self::assertSame(
             json_encode([
-                "version"     => "v1.3.0",
+                "version"     => "v1.4.0",
                 "title"       => "Cerealis API",
                 "description" => "Enterprise Resource Management API",
                 "host"        => "https://mspr.minarox.fr",
