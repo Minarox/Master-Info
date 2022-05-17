04d4a78fcbf0adaa73899516cd63fcf8e7b251c3
Mathis <contact@minarox.fr>
2022-05-17 11:40:36 +0200

    Translate Admins page

--- src/views/Admins.vue
+++ src/views/Admins.vue
@@ -17,19 +17,19 @@
         <article id="filter">
           <form @submit.prevent="getAdmins">
             <div>
-              <label for="email">Email</label>
+              <label for="email">{{ $t("email") }}</label>
               <input type="text" name="email" id="email" v-model="email">
             </div>
             <div>
-              <label for="first_name">Prénom</label>
+              <label for="first_name">{{ $t("first-name") }}</label>
               <input type="text" name="first_name" id="first_name" v-model="first_name">
             </div>
             <div>
-              <label for="last_name">Nom</label>
+              <label for="last_name">{{ $t("last-name") }}</label>
               <input type="text" name="last_name" id="last_name" v-model="last_name">
             </div>
             <div>
-              <label for="scope">Type</label>
+              <label for="scope">{{ $t("type") }}</label>
               <select name="scope" id="scope" v-model="scope">
                 <option value=""></option>
                 <option value="admin">Admin</option>
@@ -37,7 +37,7 @@
               </select>
             </div>
             <div>
-              <label for="active">Actif</label>
+              <label for="active">{{ $t("active") }}</label>
               <select name="active" id="active" v-model="active">
                 <option value=""></option>
                 <option value="1">✅</option>
@@ -55,7 +55,7 @@
 
         <article id="main">
           <header>
-            <h2>Administrateurs de l'application</h2>
+            <h2>{{ $t("app-admins") }}</h2>
             <div>
               <a href="" @click.prevent="getAdmins" class="svg-button">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M496 48V192c0 17.69-14.31 32-32 32H320c-17.69 0-32-14.31-32-32s14.31-32 32-32h63.39c-29.97-39.7-77.25-63.78-127.6-63.78C167.7 96.22 96 167.9 96 256s71.69 159.8 159.8 159.8c34.88 0 68.03-11.03 95.88-31.94c14.22-10.53 34.22-7.75 44.81 6.375c10.59 14.16 7.75 34.22-6.375 44.81c-39.03 29.28-85.36 44.86-134.2 44.86C132.5 479.9 32 379.4 32 256s100.5-223.9 223.9-223.9c69.15 0 134 32.47 176.1 86.12V48c0-17.69 14.31-32 32-32S496 30.31 496 48z"/></svg>
@@ -70,12 +70,12 @@
               <!-- Head -->
               <thead>
               <tr>
-                <th>Email</th>
-                <th>Nom complet</th>
-                <th>Type</th>
-                <th>Actif</th>
-                <th>Ajouté le</th>
-                <th>Actions</th>
+                <th>{{ $t("email") }}</th>
+                <th>{{ $t("full-name") }}</th>
+                <th>{{ $t("type") }}</th>
+                <th>{{ $t("active") }}</th>
+                <th>{{ $t("added") }}</th>
+                <th>{{ $t("actions") }}</th>
               </tr>
               </thead>
               <!-- Content -->
@@ -101,7 +101,7 @@
               </tbody>
               <tbody v-else>
               <tr>
-                <td colspan="6" class="empty-table">Aucune donnée n'a été trouvée.</td>
+                <td colspan="6" class="empty-table">{{ $t("no-data") }}</td>
               </tr>
               </tbody>
             </table>
