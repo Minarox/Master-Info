f1711493f8526811d97b2e0b12d8143d3435d00e
Etienne <titibobo0@gmail.com>
2022-05-17 16:39:42 +0200

    Rework des boutons de ShareActivity pour rediriger vers les bonnes pages

--- app/src/main/java/com/google/ar/core/examples/java/augmentedimage/ShareActivity.java
+++ app/src/main/java/com/google/ar/core/examples/java/augmentedimage/ShareActivity.java
@@ -8,6 +8,8 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
@@ -21,6 +23,8 @@ public class ShareActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
         ImageView ShowView = findViewById(R.id.ShowImg);
+        Button OtherPhoto = findViewById(R.id.OtherPhoto);
+        Button Share = findViewById(R.id.Share);
 
         Intent intent = getIntent();
         ImgString = intent.getStringExtra("Name");
@@ -30,11 +34,31 @@ public class ShareActivity extends AppCompatActivity {
         Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
 
         ShowView.setImageBitmap(myBitmap);
+
+        OtherPhoto.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ChangeActivityAI();
+            }
+        });
+
+        Share.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ChangeActivityShareForm();
+            }
+        });
     }
 
-    private void ChangeActivity(String ImgName) {
+    private void ChangeActivityAI() {
         Intent myIntent = new Intent(this, AugmentedImageActivity.class);
         startActivity(myIntent);
     }
 
+    private void ChangeActivityShareForm() {
+        Intent myIntent = new Intent(this, ShareFormActivity.class);
+        startActivity(myIntent);
+    }
 }
