dab28e85555f30321a89b8d282465b2055729def
Mathis <contact@minarox.fr>
2022-05-17 11:49:57 +0200

    Translate Statistics page

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -14,7 +14,7 @@
       <div>
         <article id="main">
           <header>
-            <h2>Statistiques</h2>
+            <h2>{{ $t("statistics") }}</h2>
             <div>
               <a href="" @click.prevent="refreshStats" class="svg-button">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M496 48V192c0 17.69-14.31 32-32 32H320c-17.69 0-32-14.31-32-32s14.31-32 32-32h63.39c-29.97-39.7-77.25-63.78-127.6-63.78C167.7 96.22 96 167.9 96 256s71.69 159.8 159.8 159.8c34.88 0 68.03-11.03 95.88-31.94c14.22-10.53 34.22-7.75 44.81 6.375c10.59 14.16 7.75 34.22-6.375 44.81c-39.03 29.28-85.36 44.86-134.2 44.86C132.5 479.9 32 379.4 32 256s100.5-223.9 223.9-223.9c69.15 0 134 32.47 176.1 86.12V48c0-17.69 14.31-32 32-32S496 30.31 496 48z"/></svg>
@@ -24,23 +24,23 @@
           <section>
             <article>
               <!-- Global statistics -->
-              <h3>Statistiques globales</h3>
+              <h3>{{ $t("global-stats") }}</h3>
               <section>
                 <table>
                   <tr>
-                    <td>Nombre total d'utilisateurs </td>
+                    <td>{{ $t("nb-users") }}</td>
                     <td>{{ stats["nb_users"] }}</td>
                   </tr>
                   <tr>
-                    <td>Nombre total de partage</td>
+                    <td>{{ $t("nb-share") }}</td>
                     <td>{{ stats["nb_share"] }}</td>
                   </tr>
                   <tr>
-                    <td>Moyenne d'utilisation par jours</td>
+                    <td>{{ $t("avg-usage-day") }}</td>
                     <td>{{ stats["avg_usage_day"] }}</td>
                   </tr>
                   <tr>
-                    <td>Moyenne d'utilisation sur les 7 derniers jours</td>
+                    <td>{{ $t("avg-usage-7-days") }}</td>
                     <td>{{ stats["avg_usage_7_days"] }}</td>
                   </tr>
                 </table>
@@ -48,21 +48,21 @@
             </article>
             <!-- Average usage per month -->
             <article>
-              <h3>Moyenne d'utilisation sur le mois</h3>
+              <h3>{{ $t("avg-usage-month") }}</h3>
               <div>
                 <canvas id="monthly-sessions"></canvas>
               </div>
             </article>
             <!-- Average usage per day -->
             <article>
-              <h3>Moyenne d'utilisation par jours</h3>
+              <h3>{{ $t("avg-usage-day") }}</h3>
               <div>
                 <canvas id="daily-sessions"></canvas>
               </div>
             </article>
             <!-- Average usage per hour -->
             <article>
-              <h3>Moyenne d'utilisation par heures</h3>
+              <h3>{{ $t("avg-usage-hour") }}</h3>
               <div>
                 <canvas id="hourly-sessions"></canvas>
               </div>
@@ -187,6 +187,9 @@ export default {
     },
     diagramDay(dataset) {
       let canvas = document.getElementById("daily-sessions").getContext("2d");
+
+      for (let i = 0; i < dataset[0].length; i++)
+        dataset[0][i] = this.$t(dataset[0][i]);
 
       new Chart(canvas, {
         type: "bar",
