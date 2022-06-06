bd801719977a86a377bf586d94d5c34361a9ccc8
Mathis S.M <mathis.sema@gmail.com>
2022-06-06 04:02:09 +0200

    Add diagram

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
           <header>
             <h2>{{ $t("passage-dog") }}</h2>
             <div>
-              <a href="" @click.prevent="" class="svg-button">
+              <a href="" @click.prevent="getPassages" class="svg-button">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                   <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
                   <path
@@ -19,7 +19,12 @@
             </div>
           </header>
           <section>
-            <p>Histogram</p>
+            <div v-if="passages" class="canvas-container">
+              <canvas id="passages-canvas"></canvas>
+            </div>
+            <div v-else class="canvas-container">
+              <p>{{ $t("nothing-found") }}</p>
+            </div>
           </section>
         </article>
       </div>
@@ -28,12 +33,87 @@
 </template>
 
 <script>
-import Header from "@/components/global/Header";
+import Header from "../components/global/Header";
+import { API } from "../assets/js/api";
+import Chart from "chart.js";
 
 export default {
   name: "Users",
   components: {
     Header,
+  },
+  data() {
+    return {
+      passages: [],
+      food: [],
+    };
+  },
+  mounted() {
+    this.getPassages();
+    this.getFoods();
+  },
+  methods: {
+    getPassages() {
+      API.getPassages()
+        .then((response) => {
+          this.passages = response;
+          this.diagram(response);
+        })
+        .catch(() => {
+          this.passages = null;
+        });
+    },
+    getFoods() {
+      API.getFoods().then((response) => {
+        this.food = response[0];
+      });
+    },
+    parseDate(date) {
+      date = new Date(date).toLocaleString().split(" ");
+      let time = date[1].split(":");
+      time = time[0] + "h" + time[1].slice(0, 1) + "0";
+      return date[0] + " " + time;
+    },
+    diagram(data) {
+      let canvas = document.getElementById("passages-canvas").getContext("2d");
+
+      let colorGradient = canvas.createLinearGradient(0, 230, 0, 50);
+      colorGradient.addColorStop(0, "rgb(20, 168, 159)");
+      colorGradient.addColorStop(1, "rgb(35, 170, 225)");
+
+      let dataset = [[], []];
+
+      for (let i = 0; i < 18; i++) {
+        let date = this.parseDate(new Date().getTime() - i * 10 * 60000);
+        dataset[0][i] = date.split(" ")[1];
+        dataset[1][i] = 0;
+
+        for (let y = 0; y < data.length; y++) {
+          if (this.parseDate(data[y]["seen_at"]) === date) {
+            dataset[1][i] += 1;
+          }
+        }
+      }
+
+      new Chart(canvas, {
+        type: "line",
+        data: {
+          labels: dataset[0].reverse(),
+          datasets: [
+            {
+              label: this.$t("sessions"),
+              data: dataset[1].reverse(),
+              backgroundColor: colorGradient,
+            },
+          ],
+        },
+        options: {
+          maintainAspectRatio: false,
+          legend: { display: false },
+          scales: { yAxes: [{ ticks: { beginAtZero: true } }] },
+        },
+      });
+    },
   },
 };
 </script>
@@ -41,5 +121,18 @@ export default {
 <style scoped>
 #main > section {
   height: calc(100vh - 212px);
+}
+
+.canvas-container {
+  position: relative;
+  padding: 0 6px 4px 2px;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.canvas-container > p {
+  text-align: center;
+  padding-top: 30px;
 }
 </style>
