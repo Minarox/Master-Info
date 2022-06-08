bc3c3278564a9a77aecb4bafb661866a9a52ab3e
Mathis <contact@minarox.fr>
2022-06-08 18:09:41 +0200

    Fix order by

--- tests/Controllers/UserControllerTest.php
+++ tests/Controllers/UserControllerTest.php
@@ -63,7 +63,7 @@ class UserControllerTest extends TestCase
         self::assertSame(
             json_encode(
                 $GLOBALS["pdo"]
-                    ->query("SELECT user_id, email, first_name, last_name, device, nb_share, created_at FROM users ORDER BY first_name LIMIT 300;")
+                    ->query("SELECT user_id, email, first_name, last_name, device, nb_share, created_at FROM users ORDER BY created_at DESC LIMIT 300;")
                     ->fetchAll()
             ),
             $result->getBody()->__toString()
